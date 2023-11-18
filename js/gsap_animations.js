5a79ba085c90ef683882cf7005c658ce42089278
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-11-19 02:52:53 +0800

    gsap and new mobile animation

---
+++ js/gsap_animations.js
@@ -0,0 +1,25 @@
+gsap.fromTo(`#gsap_author`,
+            { y : "-100px" , x:"-100px",  color: "#10ff48", rotation: 0 ,scale : 10  },
+            {
+                x: "0px" ,
+                y : "0px",
+                color: "#000000" , 
+                duration: 3,
+                ease: "bounce",
+                rotation: 360,
+                scale: 1,
+
+            })
+setTimeout(function(){
+    gsap.fromTo(`#gsap_content`,
+            { y : "-100px" , x:"-100px"},
+            {
+                x: "0px" ,
+                y : "0px",
+                display : "block", 
+                duration: 3 ,
+                ease: "bounce",
+
+
+            })
+} , 3000 ) 
