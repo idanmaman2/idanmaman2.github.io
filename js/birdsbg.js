b98488a0b71c0f1271e97798c2ef723e2cad050e
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-11-26 22:35:10 +0200

    neckless

--- js/birdsbg.js
+++ js/birdsbg.js
@@ -5,11 +5,13 @@ import { GLTFLoader } from 'GLTFLoader';
 if(!window.matchMedia("(max-width: 600px)").matches) {
   const myCanvas = document.querySelector('#myCanvas');
   var clock = new THREE.Clock();
-  var mixer1,mixer2,mixer3 ; 
+  var mixer1,mixer2,mixer3 , mixer4 ; 
   var hat ; 
+  var neckless ; 
   var parrot  = [] ; 
   var sx,sy,sz ; 
   var shx,shy,shz ;
+  var snx,sny,snz ;
   var factor = 1 ; 
   var speed = 3000 ; 
   var radius = 100 ; 
@@ -54,23 +56,27 @@ if(!window.matchMedia("(max-width: 600px)").matches) {
   renderer.autoClear = false;
 
     const loader = new GLTFLoader();
-    loader.load('/css/threejs/model/hat_z_parrot.glb', function (gltf) {
+    loader.load('/css/threejs/model/hat_x_parrot.glb', function (gltf) {
       const model1 = gltf.scene.children[0];
       const model2 = model1.clone(); // Clone the first model
       const model3 = gltf.scene.children[1];
+      const model4 = gltf.scene.children[2];
     
       // Clone the animation clip for each model
       const clip1 = gltf.animations[0].clone();
       const clip2 = clip1.clone();
       const clip3 =  gltf.animations[1].clone();
+      const clip4 =  gltf.animations[2].clone();
     
        mixer1 = new THREE.AnimationMixer(model1);
        mixer2 = new THREE.AnimationMixer(model2);
        mixer3 = new THREE.AnimationMixer(model3);
+       mixer4 = new THREE.AnimationMixer(model4);
         
       mixer1.clipAction(clip1).play();
       mixer2.clipAction(clip2).play();
       mixer3.clipAction(clip3).play();
+      mixer4.clipAction(clip4).play();
     
       const wireframeMaterial = new THREE.MeshBasicMaterial({color : 0xff0000 , wireframe: true})
     
@@ -79,11 +85,13 @@ if(!window.matchMedia("(max-width: 600px)").matches) {
       // Add both models to the parrots array
       parrot.push(model1, model2);
       hat = model3 ; 
+      neckless = model4 ; 
       orbitControls.target.copy(model1.position);
     
       scene.add(model1);
       scene.add(model2);
       scene.add(model3);
+      scene.add(model4);
     
       sx = model1.position.x;
       sy = model1.position.y;
@@ -92,6 +100,10 @@ if(!window.matchMedia("(max-width: 600px)").matches) {
       shx = model3.position.x;
       shy = model3.position.y;
       shz = model3.position.z;
+
+      snx = model4.position.x;
+      sny = model4.position.y;
+      snz = model4.position.z;
 
 
 
@@ -135,7 +147,7 @@ if(!window.matchMedia("(max-width: 600px)").matches) {
     var [angle1 , angle2 ] = [time  , time] ; 
 
 
-    if ( mixer1 && mixer2 && mixer3 ) {
+    if ( mixer1 && mixer2 && mixer3 && mixer4) {
         mixer1.update( delta );
         mixer2.update( delta );
         mixer3.update( delta );
@@ -149,6 +161,12 @@ if(!window.matchMedia("(max-width: 600px)").matches) {
         hat.position.x = shx + shift_x + Math.cos(angle1) * radius ;
         hat.position.y= shy + shift_y + Math.sin(angle1) *  radius ; 
         hat.position.z = shz + Math.sin(angle1) * radius ; 
+
+        neckless.position.x = snx + shift_x + Math.cos(angle1) * radius ;
+        neckless.position.y= sny + shift_y + Math.sin(angle1) *  radius ; 
+        neckless.position.z = snz + Math.sin(angle1) * radius ; 
+
+         
        // hat.rotation.x =  Math.cos(angle1) * Math.PI/3
        // hat.rotation.y =  Math.cos(angle1 + Math.random() * Math.PI / 100) * Math.PI/6 
 
